7dcc62d850a5f503ef64eb2c59d47df26af611b5
David Dieperink <david.dieperink@ik.me>
2022-12-20 20:57:56 +0100

    adding a footballLeague list + implements OnInit interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    template: `<h1>Welcome to {{title}}!</h1>`
+    template: `<h1>Welcome to {{footballLeagueList[0]}}!</h1>`
 })
-export class AppComponent {
-    title = 'angular-app';
+export class AppComponent implements OnInit{
+    footballLeagueList = ['Premier League', 'Ligue A', 'Ligue 1', 'Bundesliga'];
+
+    ngOnInit(): void {
+        console.table(this.footballLeagueList)
+    }
 }
