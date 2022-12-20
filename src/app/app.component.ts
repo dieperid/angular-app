1170a0aa0ba9684ba69404cc579bb3255bee7b25
David Dieperink <david.dieperink@ik.me>
2022-12-20 21:09:00 +0100

    create a new function to display football league name and adding comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,23 @@ import {Component, OnInit} from '@angular/core';
     selector: 'app-root',
     template: `<h1>Welcome to {{footballLeagueList[0]}}!</h1>`
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
+    // Creating a list of the football leagues
     footballLeagueList = ['Premier League', 'Ligue A', 'Ligue 1', 'Bundesliga'];
 
+    /**
+     * Implements OnInit function from the OnInit interface
+     */
     ngOnInit(): void {
         console.table(this.footballLeagueList)
+        this.selectFootballLeague(this.footballLeagueList[0])
+    }
+
+    /**
+     * Function to display a football league name
+     * @param footballLeagueName
+     */
+    selectFootballLeague(footballLeagueName: string) {
+        console.log(`You have clicked on the football league ${footballLeagueName}`)
     }
 }
