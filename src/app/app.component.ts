9fba0a5b2bda2f14c22fd1b66de82788333e0168
David Dieperink <david.dieperink@ik.me>
2022-12-21 21:10:18 +0100

    importing the list of football club + the model | get the football club list and send it to the OnInit method | show the name of the club when a club is selected

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,30 @@
 import { Component, OnInit } from "@angular/core";
+import { FOOTBALLCLUBS } from "./mock-football-club";
+import { FootballClub } from "./football-club";
 
 @Component({
     selector: "app-root",
-    template: `<h1>Welcome to {{ footballLeagueList[0] }}!</h1>`,
+    template: `<h1>List of football club</h1>`,
 })
 export class AppComponent implements OnInit {
-    // Creating a list of the football leagues
-    footballLeagueList = ["Premier League", "Ligue A", "Ligue 1", "Bundesliga"];
+    // Creating a list of the football club
+    footballClubList = FOOTBALLCLUBS;
 
     /**
      * Implements OnInit function from the OnInit interface
      */
     ngOnInit(): void {
-        console.table(this.footballLeagueList);
-        this.selectFootballLeague(this.footballLeagueList[0]);
+        console.table(this.footballClubList);
+        this.selectFootballClub(this.footballClubList[0]);
     }
 
     /**
-     * Function to display a football league name
-     * @param footballLeagueName
+     * Function to display a football club name
+     * @param footballClubName
      */
-    selectFootballLeague(footballLeagueName: string) {
+    selectFootballClub(footballClub: FootballClub) {
         console.log(
-            `You have clicked on the football league ${footballLeagueName}`
+            `You have clicked on the football league ${footballClub.name}`
         );
     }
 }
