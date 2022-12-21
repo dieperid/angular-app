2627afe4cdb5765b09b88112611bb1f1f873f2fc
David Dieperink <david.dieperink@ik.me>
2022-12-21 19:15:20 +0100

    update prettier settings and indent the code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,19 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-    selector: 'app-root',
-    template: `<h1>Welcome to {{footballLeagueList[0]}}!</h1>`
+    selector: "app-root",
+    template: `<h1>Welcome to {{ footballLeagueList[0] }}!</h1>`,
 })
 export class AppComponent implements OnInit {
     // Creating a list of the football leagues
-    footballLeagueList = ['Premier League', 'Ligue A', 'Ligue 1', 'Bundesliga'];
+    footballLeagueList = ["Premier League", "Ligue A", "Ligue 1", "Bundesliga"];
 
     /**
      * Implements OnInit function from the OnInit interface
      */
     ngOnInit(): void {
-        console.table(this.footballLeagueList)
-        this.selectFootballLeague(this.footballLeagueList[0])
+        console.table(this.footballLeagueList);
+        this.selectFootballLeague(this.footballLeagueList[0]);
     }
 
     /**
@@ -21,6 +21,8 @@ export class AppComponent implements OnInit {
      * @param footballLeagueName
      */
     selectFootballLeague(footballLeagueName: string) {
-        console.log(`You have clicked on the football league ${footballLeagueName}`)
+        console.log(
+            `You have clicked on the football league ${footballLeagueName}`
+        );
     }
 }
