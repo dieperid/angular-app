7bfc33fb90a993a687f55f66762c25f4aa8da1b8
David Dieperink <david.dieperink@ik.me>
2022-12-27 12:18:06 +0100

    adding 3 variables to set default value (color, border height) + adding comment

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -4,20 +4,40 @@ import { Directive, ElementRef, HostListener, Input } from "@angular/core";
 	selector: "[ftblBorderCard]",
 })
 export class BorderCardDirective {
+	private initialColor: string = "#f5f5f5";
+	private defaultColor: string = "#009688";
+	private defaultHeight: number = 180;
+
 	constructor(private el: ElementRef) {
-		this.setHeight(180);
-		this.setBorder("#f5f5f5");
+		this.setHeight(this.defaultHeight);
+		this.setBorder(this.initialColor);
 	}
 
+	/**
+	 * Take the input from the html to get the border color
+	 *
+	 * @type {string}
+	 * @memberof BorderCardDirective
+	 */
 	@Input("ftblBorderCard") borderColor: string;
 
+	/**
+	 * HostListener to record the mouse enter event
+	 *
+	 * @memberof BorderCardDirective
+	 */
 	@HostListener("mouseenter")
 	onMouseEnter() {
-		this.setBorder(this.borderColor || "#009688");
+		this.setBorder(this.borderColor || this.defaultColor);
 	}
 
+	/**
+	 * HostListener to record the mouse leave event
+	 *
+	 * @memberof BorderCardDirective
+	 */
 	@HostListener("mouseleave") onMouseLeave() {
-		this.setBorder("#f5f5f5");
+		this.setBorder(this.initialColor);
 	}
 
 	/**
