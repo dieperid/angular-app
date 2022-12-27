9289d5d9f67a161f1d9a9c71d5d11044c3b4de31
David Dieperink <david.dieperink@ik.me>
2022-12-27 11:52:23 +0100

    created setHeight to set the height of the border + setBorder to set the color of the border

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -1,8 +1,31 @@
-import { Directive } from "@angular/core";
+import { Directive, ElementRef } from "@angular/core";
 
 @Directive({
 	selector: "[appBorderCard]",
 })
 export class BorderCardDirective {
-	constructor() {}
+	constructor(private el: ElementRef) {
+		this.setHeight(180);
+		this.setBorder("#f5f5f5");
+	}
+
+	/**
+	 * Function to set the height of the border card
+	 *
+	 * @param {number} height
+	 * @memberof BorderCardDirective
+	 */
+	setHeight(height: number) {
+		this.el.nativeElement.style.height = `${height}px`;
+	}
+
+	/**
+	 * Function to set the color of the border card
+	 *
+	 * @param {string} color
+	 * @memberof BorderCardDirective
+	 */
+	setBorder(color: string) {
+		this.el.nativeElement.style.border = `solid 4px ${color}`;
+	}
 }
