360c2adbb0a90bccb5e5b94b6848486de036f1ab
dieperid <david.dieperink@ik.me>
2022-12-29 17:10:44 +0100

    added header comment + switch for the different color for the leagues

--- src/app/football-club-league-color.pipe.ts
+++ src/app/football-club-league-color.pipe.ts
@@ -1,10 +1,39 @@
 import { Pipe, PipeTransform } from "@angular/core";
-
+/**
+ * Display the color of the football club league
+ * Take the league in argument
+ *
+ * @export
+ * @class FootballClubLeagueColorPipe
+ * @implements {PipeTransform}
+ */
 @Pipe({
 	name: "footballClubLeagueColor",
 })
 export class FootballClubLeagueColorPipe implements PipeTransform {
-	transform(value: unknown, ...args: unknown[]): unknown {
-		return null;
+	transform(league: string): string {
+		let color: string;
+
+		switch (league) {
+			case "LaLiga":
+				color = "red lighten-1";
+				break;
+			case "Ligue 1":
+				color = "blue lighten-1";
+				break;
+			case "Premier League":
+				color = "green lighten-1";
+				break;
+			case "Bundesliga":
+				color = "brown lighten-1";
+				break;
+			case "Serie A":
+				color = "grey lighten-3";
+				break;
+			default:
+				color = "grey";
+				break;
+		}
+		return "chip " + color;
 	}
 }
