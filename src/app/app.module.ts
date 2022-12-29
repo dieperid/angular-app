f94b7f551f018e3c4da000668ad8aae525e5609e
dieperid <david.dieperink@ik.me>
2022-12-29 16:50:22 +0100

    added pipe file for the club league color

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BorderCardDirective } from "./border-card.directive";
+import { FootballClubLeagueColorPipe } from './football-club-league-color.pipe';
 
 @NgModule({
-	declarations: [AppComponent, BorderCardDirective],
+	declarations: [AppComponent, BorderCardDirective, FootballClubLeagueColorPipe],
 	imports: [BrowserModule, AppRoutingModule],
 	providers: [],
 	bootstrap: [AppComponent],
