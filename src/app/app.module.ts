8f002db162a7654f596b30b8f732615c88461fc3
dieperid <david.dieperink@ik.me>
2022-12-30 17:57:36 +0100

    generated 2 new component, -> display the details of a club, -> display the list of club

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BorderCardDirective } from "./border-card.directive";
-import { FootballClubLeagueColorPipe } from './football-club-league-color.pipe';
+import { FootballClubLeagueColorPipe } from "./football-club-league-color.pipe";
+import { ListFootballClubComponent } from "./list-football-club/list-football-club.component";
+import { DetailFootballClubComponent } from './detail-football-club/detail-football-club.component';
 
 @NgModule({
-	declarations: [AppComponent, BorderCardDirective, FootballClubLeagueColorPipe],
+	declarations: [
+		AppComponent,
+		BorderCardDirective,
+		FootballClubLeagueColorPipe,
+		ListFootballClubComponent,
+  DetailFootballClubComponent,
+	],
 	imports: [BrowserModule, AppRoutingModule],
 	providers: [],
 	bootstrap: [AppComponent],
---
+++ src/app/list-football-club/list-football-club.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: "app-list-football-club",
+	templateUrl: "./list-football-club.component.html",
+	styles: [],
+})
+export class ListFootballClubComponent {}
