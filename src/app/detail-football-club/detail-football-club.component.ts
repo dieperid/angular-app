cb24b208278ca4f40c7a536c3bbc0bf84e41615d
dieperid <david.dieperink@ik.me>
2022-12-30 18:50:35 +0100

    get the id and get the specific club with is id

--- src/app/detail-football-club/detail-football-club.component.ts
+++ src/app/detail-football-club/detail-football-club.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { FootballClub } from "../football-club";
+import { FOOTBALLCLUBS } from "../mock-football-club";
 
 @Component({
 	selector: "app-detail-football-club",
@@ -6,7 +9,21 @@ import { Component, OnInit } from "@angular/core";
 	styles: [],
 })
 export class DetailFootballClubComponent implements OnInit {
-	constructor() {}
+	footballClubList: FootballClub[];
+	footballClub: FootballClub;
 
-	ngOnInit() {}
+	constructor(private route: ActivatedRoute) {}
+
+	ngOnInit() {
+		this.footballClubList = FOOTBALLCLUBS;
+
+		// get the id of the club with the route
+		const clubId: number | null = Number(
+			this.route.snapshot.paramMap.get("id")
+		);
+		// get the club and find him in the list of club with the id
+		const club: FootballClub | undefined = this.footballClubList.find(
+			(club) => club.id === clubId
+		);
+	}
 }
