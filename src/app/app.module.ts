a65c217257be1453a1e2de59b051d8df3825d7fe
dieperid <david.dieperink@ik.me>
2022-12-30 18:03:26 +0100

    making good indent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from "./app.component";
 import { BorderCardDirective } from "./border-card.directive";
 import { FootballClubLeagueColorPipe } from "./football-club-league-color.pipe";
 import { ListFootballClubComponent } from "./list-football-club/list-football-club.component";
-import { DetailFootballClubComponent } from './detail-football-club/detail-football-club.component';
+import { DetailFootballClubComponent } from "./detail-football-club/detail-football-club.component";
 
 @NgModule({
 	declarations: [
@@ -14,7 +14,7 @@ import { DetailFootballClubComponent } from './detail-football-club/detail-footb
 		BorderCardDirective,
 		FootballClubLeagueColorPipe,
 		ListFootballClubComponent,
-  DetailFootballClubComponent,
+		DetailFootballClubComponent,
 	],
 	imports: [BrowserModule, AppRoutingModule],
 	providers: [],
