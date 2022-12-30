e50b32ce03c9a165fa1d566c89d3dbc1cd689614
dieperid <david.dieperink@ik.me>
2022-12-30 18:14:49 +0100

    creating 3 routes for the project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { DetailFootballClubComponent } from "./detail-football-club/detail-football-club.component";
+import { ListFootballClubComponent } from "./list-football-club/list-football-club.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: "/football-clubs", component: ListFootballClubComponent },
+	{ path: "/football-clubs/:id", component: DetailFootballClubComponent },
+	{ path: "", redirectTo: "/football-clubs", pathMatch: "full" },
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
