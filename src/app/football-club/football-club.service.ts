22ab022aa458cf5f5af704ccb6d78d1cc496d6ba
David Dieperink <david.dieperink@ik.me>
2022-12-31 12:05:36 +0100

    created three function, 1. get the list of the club, 2. get a club by his id, 3. get all the leagues

--- src/app/football-club/football-club.service.ts
+++ src/app/football-club/football-club.service.ts
@@ -1,8 +1,41 @@
 import { Injectable } from "@angular/core";
+import { FootballClub } from "./football-club";
+import { FOOTBALLCLUBS } from "./mock-football-club";
 
 @Injectable({
 	providedIn: "root",
 })
 export class FootballClubService {
-	constructor() {}
+	/**
+	 * Function to get the list of the football club
+	 *
+	 * @return {*}  {FootballClub[]}
+	 * @memberof FootballClubService
+	 */
+	getFootballClubList(): FootballClub[] {
+		return FOOTBALLCLUBS;
+	}
+
+	/**
+	 * Function to a football club by his id
+	 *
+	 * @param {number} footballClubId
+	 * @return {*}  {(FootballClub | undefined)}
+	 * @memberof FootballClubService
+	 */
+	getFootballClubById(footballClubId: number): FootballClub | undefined {
+		return FOOTBALLCLUBS.find(
+			(footballClub) => footballClub.id == footballClubId
+		);
+	}
+
+	/**
+	 * Function to get the leagues of the football clubs
+	 *
+	 * @return {*}  {string[]}
+	 * @memberof FootballClubService
+	 */
+	getFootballClubLeague(): string[] {
+		return ["Ligue 1", "LaLiga", "PL", "Bundesliga", "Serie A"];
+	}
 }
