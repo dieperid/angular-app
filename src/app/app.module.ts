66f108d322f757fac88bc2a41ea2853b60735559
David Dieperink <david.dieperink@ik.me>
2022-12-31 11:31:42 +0100

    updated import form the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,12 @@ import { PageNotFoundComponent } from "./page-not-found/page-not-found.component
 
 @NgModule({
 	declarations: [AppComponent, PageNotFoundComponent],
-	imports: [BrowserModule, AppRoutingModule, FootballClubModule],
+	/**
+	 * Angular load the module in order (from Browser to AppRouting)
+	 * So we have to load FootballClubModule before AppRoutingModule
+	 * We have to load the football club before the routes
+	 */
+	imports: [BrowserModule, FootballClubModule, AppRoutingModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
