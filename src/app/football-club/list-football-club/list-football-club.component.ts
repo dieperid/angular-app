0445df25e70fb3ec792978de1de938360f671d40
David Dieperink <david.dieperink@ik.me>
2022-12-31 12:21:05 +0100

    instantiate the football-club service in the list football club component to have access to the football club list

--- src/app/football-club/list-football-club/list-football-club.component.ts
+++ src/app/football-club/list-football-club/list-football-club.component.ts
@@ -1,17 +1,25 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { FootballClub } from "../football-club";
-import { FOOTBALLCLUBS } from "../mock-football-club";
+import { FootballClubService } from "../football-club.service";
 
 @Component({
 	selector: "app-list-football-club",
 	templateUrl: "./list-football-club.component.html",
 })
-export class ListFootballClubComponent {
+export class ListFootballClubComponent implements OnInit {
 	// Creating a list of the football club
-	footballClubList: FootballClub[] = FOOTBALLCLUBS;
+	footballClubList: FootballClub[];
 
-	constructor(private router: Router) {}
+	constructor(
+		private router: Router,
+		private footballService: FootballClubService
+	) {}
+
+	ngOnInit() {
+		// get the list of the club from the Service
+		this.footballClubList = this.footballService.getFootballClubList();
+	}
 
 	goToFootballClub(footballClub: FootballClub) {
 		this.router.navigate(["/football-club", footballClub.id]);
