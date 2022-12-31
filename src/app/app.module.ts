1acf467f8d00dff33cc87eff072b7a57bcdfa927
David Dieperink <david.dieperink@ik.me>
2022-12-31 11:27:12 +0100

    added file that concern football club in the football club module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,12 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { BorderCardDirective } from "./border-card.directive";
-import { FootballClubLeagueColorPipe } from "./football-club-league-color.pipe";
-import { ListFootballClubComponent } from "./list-football-club/list-football-club.component";
-import { DetailFootballClubComponent } from "./detail-football-club/detail-football-club.component";
+import { FootballClubModule } from "./football-club/football-club.module";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 
 @NgModule({
-	declarations: [
-		AppComponent,
-		BorderCardDirective,
-		FootballClubLeagueColorPipe,
-		ListFootballClubComponent,
-		DetailFootballClubComponent,
-		PageNotFoundComponent,
-	],
-	imports: [BrowserModule, AppRoutingModule],
+	declarations: [AppComponent, PageNotFoundComponent],
+	imports: [BrowserModule, AppRoutingModule, FootballClubModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
