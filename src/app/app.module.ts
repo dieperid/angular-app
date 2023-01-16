b443a6f736c5c36f6edd1a4d17cbb7f43d0cef39
dieperid <david.dieperink@ik.me>
2023-01-16 13:53:00 +0100

    added all the dependancies to the main module of the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 import { HttpClientModule } from "@angular/common/http";
+import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { FootballClubModule } from "./football-club/football-club.module";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { InMemoryDataService } from "./in-memory-data.service";
 
 @NgModule({
 	declarations: [AppComponent, PageNotFoundComponent],
@@ -19,6 +21,9 @@ import { PageNotFoundComponent } from "./page-not-found/page-not-found.component
 		BrowserModule,
 		FormsModule,
 		HttpClientModule,
+		HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+			dataEncapsulation: false,
+		}),
 		FootballClubModule,
 		AppRoutingModule,
 	],
