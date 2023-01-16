5cc3a083cd31823b335909006c518ee07d7c32cc
dieperid <david.dieperink@ik.me>
2023-01-16 15:39:36 +0100

    Created handleError function + log function + updated the club service function

--- src/app/football-club/football-club.service.ts
+++ src/app/football-club/football-club.service.ts
@@ -2,7 +2,6 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { catchError, Observable, of, tap } from "rxjs";
 import { FootballClub } from "./football-club";
-import { FOOTBALLCLUBS } from "./mock-football-club";
 
 @Injectable()
 export class FootballClubService {
@@ -16,11 +15,8 @@ export class FootballClubService {
 	 */
 	getFootballClubList(): Observable<FootballClub[]> {
 		return this.http.get<FootballClub[]>("api/football-clubs").pipe(
-			tap((footballClubList: any) => console.table(footballClubList)),
-			catchError((error) => {
-				console.log(error);
-				return of([]);
-			})
+			tap((response) => this.log(response)),
+			catchError((error) => this.handleError(error, []))
 		);
 	}
 
@@ -37,12 +33,34 @@ export class FootballClubService {
 		return this.http
 			.get<FootballClub>(`api/football-clubs/${footballClubId}`)
 			.pipe(
-				tap((footballClub) => console.log(footballClub)),
-				catchError((error) => {
-					console.log(error);
-					return of(undefined);
-				})
+				tap((response) => this.log(response)),
+				catchError((error) => this.handleError(error, undefined))
 			);
+	}
+
+	/**
+	 * Function to return an error message
+	 *
+	 * @private
+	 * @param {Error} error
+	 * @param {([] | undefined)} errorValue
+	 * @return {*}
+	 * @memberof FootballClubService
+	 */
+	private handleError(error: Error, errorValue: any) {
+		console.error(error);
+		return of(errorValue);
+	}
+
+	/**
+	 * Function to log a response
+	 *
+	 * @private
+	 * @param {(FootballClub[] | FootballClub | undefined)} response
+	 * @memberof FootballClubService
+	 */
+	private log(response: FootballClub[] | FootballClub | undefined) {
+		console.table(response);
 	}
 
 	/**
