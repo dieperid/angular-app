a0ab55b83b8c998260ed20720fa7245e81395534
dieperid <david.dieperink@ik.me>
2023-01-18 11:05:40 +0100

    added default value on the football club model

--- src/app/football-club/football-club.ts
+++ src/app/football-club/football-club.ts
@@ -1,8 +1,21 @@
 export class FootballClub {
-	id: number;
 	name: string;
 	picture: string;
-	players: Array<string>;
-	leagues: Array<string>;
+	players: string[];
+	leagues: string[];
 	created: Date;
+
+	constructor(
+		name: string = "Enter a name",
+		picture: string = "",
+		players: string[] = [""],
+		leagues: string[] = [""],
+		created: Date = new Date()
+	) {
+		this.name = name;
+		this.picture = picture;
+		this.players = players;
+		this.leagues = leagues;
+		this.created = created;
+	}
 }
