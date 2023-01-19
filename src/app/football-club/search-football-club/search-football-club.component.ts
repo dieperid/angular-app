c7b43d6368f89e6c4bc66b0e40cb95599020fc64
dieperid <david.dieperink@ik.me>
2023-01-19 15:23:51 +0100

    implement the search function in OnInit

--- src/app/football-club/search-football-club/search-football-club.component.ts
+++ src/app/football-club/search-football-club/search-football-club.component.ts
@@ -1,24 +1,40 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
-import { Observable, Subject } from "rxjs";
+import {
+	debounceTime,
+	distinctUntilChanged,
+	Observable,
+	Subject,
+	switchMap,
+} from "rxjs";
 import { FootballClub } from "../football-club";
+import { FootballClubService } from "../football-club.service";
 
 @Component({
 	selector: "app-search-football-club",
 	templateUrl: "./search-football-club.component.html",
 })
 export class SearchFootballClubComponent implements OnInit {
-	// example of a search
-	// {..."a".."ab"..."abs"}
 	searchParams = new Subject<string>();
-
-	// {...footballClubList(a)...footballClubList(ab)...}
 	footballClubs$: Observable<FootballClub[]>;
 
-	constructor(private router: Router) {}
+	constructor(
+		private router: Router,
+		private footballClubService: FootballClubService
+	) {}
 
 	ngOnInit(): void {
-		throw new Error("Method not implemented.");
+		this.footballClubs$ = this.searchParams.pipe(
+			// {..."a"."ab"..."abs"."ab"..."abs"}
+			debounceTime(300),
+			// {..."ab"..."ab"..."abs"}
+			distinctUntilChanged(),
+			// {..."ab"......."abs"}
+			switchMap((param) =>
+				this.footballClubService.searchFootballClubList(param)
+			)
+			// {...footballClubList(ab)...footballClubList(abs)...}
+		);
 	}
 
 	/**
@@ -40,5 +56,6 @@ export class SearchFootballClubComponent implements OnInit {
 	 */
 	goToDetailFootballClub(footballClub: FootballClub) {
 		const link = ["/football-club", footballClub.id];
+		this.router.navigate(link);
 	}
 }
