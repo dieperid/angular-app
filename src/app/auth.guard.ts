798b400645dc561e5bfc691bb1913a1fc544ae19
dieperid <david.dieperink@ik.me>
2023-01-20 08:52:24 +0100

    updated canActivate function to check if the user is logged

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,12 +1,18 @@
 import { Injectable } from "@angular/core";
-import { CanActivate } from "@angular/router";
+import { CanActivate, Router } from "@angular/router";
+import { AuthService } from "./auth.service";
 
 @Injectable({
 	providedIn: "root",
 })
 export class AuthGuard implements CanActivate {
+	constructor(private authService: AuthService, private router: Router) {}
+
 	canActivate() {
-		console.log("Guard is activated");
-		return true;
+		if (this.authService.isLoggedIn) {
+			return true;
+		}
+		this.router.navigate(["/login"]);
+		return false;
 	}
 }
