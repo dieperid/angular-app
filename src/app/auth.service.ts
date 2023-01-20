5c9f8694f43d35c81ce6cae325b908c2481447ff
dieperid <david.dieperink@ik.me>
2023-01-20 08:49:38 +0100

    login and logout function

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from "@angular/core";
+import { delay, Observable, of, tap } from "rxjs";
 
 @Injectable({
 	providedIn: "root",
 })
 export class AuthService {
-	constructor() {}
+	isLoggedIn: boolean = false;
+	redirectUrl: string;
+
+	login(username: string, password: string): Observable<boolean> {
+		const isLoggedIn = username == "dieperid" && password == "dieperid";
+
+		return of(isLoggedIn).pipe(
+			delay(1000),
+			tap((isLoggedIn) => (this.isLoggedIn = isLoggedIn))
+		);
+	}
+
+	logout() {
+		this.isLoggedIn = false;
+	}
 }
